8366518602b01e936e2072b19d6906d9e4f9ec12
Dalin Ezequiel <dalinezequiel44@gmail.com>
2025-10-29 11:18:42 +0200

    chore: init

---
+++ src/main/java/com/uem/sigapi/SigapiApplication.java
@@ -0,0 +1,13 @@
+package com.uem.sigapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uem/sigapi/SigapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uem.sigapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
