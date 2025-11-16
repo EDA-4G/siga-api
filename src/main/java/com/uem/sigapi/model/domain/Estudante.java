c4b99ae35422af76c818ad2a9bb05fe1fb2092b4
Salimo Aualia <juniorsalimoaualia@gmail.com>
2025-11-17 01:35:41 +0200

    chore: init

---
+++ src/main/java/com/uem/sigapi/model/domain/Estudante.java
@@ -0,0 +1,20 @@
+package com.uem.sigapi.model.domain;
+import lombok.*;
+import jakarta.persistence.Entity;
+@Getter
+@Setter
+@AllArgsConstructor
+
+//@Entity(name="estudantes")
+public class Estudante {
+    private String nome;
+    private String apelido;
+    private String email;
+    private String telefone;
+    private String genero;
+    private String dataNascimento;
+    
+
+    
+}
+
