c1a71e3a9ee464785bfc8da9d0ef7323c04bd7f5
Salimo Aualia <juniorsalimoaualia@gmail.com>
2025-11-17 13:44:49 +0200

    fix: corrigir DepartamentoRepo, ajustes nos métodos e inicialização

--- src/main/java/com/uem/sigapi/repository/DepartamentoRepo.java
+++ src/main/java/com/uem/sigapi/repository/DepartamentoRepo.java
@@ -1,20 +1,34 @@
 package com.uem.sigapi.repository;
+
 import com.uem.sigapi.model.domain.Departamento;
 import java.util.ArrayList;
 import java.util.List;
-public class DepartamentoRepo {
-    public static List<Departamento> departamentos = new ArrayList<>();
 
-    static{
+public class DepartamentoRepo {
+
+    private static List<Departamento> departamentos = new ArrayList<>();
+
+    static {
         departamentos.add(new Departamento("Cadeiras Gerais", "Matérias básicas de engenharia"));
         departamentos.add(new Departamento("Civil", "Engenharia Civil"));
-        departamentos.add(new Departamento("Electrotécnica", "Engenharia Eléctrica, Engenharia Eletronica, Engenharia Informatica"));
-        departamentos.add(new Departamento("Geologia", "Para todos os cursos"));
-        departamentos.add(new Departamento("Mecânica", "Engenharia de Gestao Industrial, Engenharia Mecânica"));
-        departamentos.add(new Departamento("Química", "Engenharia Ambiental, Engenharia Química"));
-
+        departamentos.add(new Departamento("Electrotécnica", "Engenharia Eléctrica"));
+        departamentos.add(new Departamento("Geologia", "Geologia para todos os cursos"));
+        departamentos.add(new Departamento("Mecânica", "Engenharia Mecânica"));
+        departamentos.add(new Departamento("Química", "Engenharia Química"));
     }
-    public static Departamento findDepartamento(String nome) {
-        return departamentos.stream().filter(d -> d.getNome().equals(nome)).findFirst().orElse(null);
+
+    public void save(Departamento departamento) {
+        departamentos.add(departamento);
+    }
+
+    public List<Departamento> findAll() {
+        return departamentos;
+    }
+
+    public Departamento findByNome(String nome) {
+        return departamentos.stream()
+                .filter(d -> d.getNome().equals(nome))
+                .findFirst()
+                .orElse(null);
     }
 }
