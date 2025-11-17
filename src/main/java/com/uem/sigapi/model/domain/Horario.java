c10d31ec3c698249aa78f71b81227ec4f1ae94f0
Salimo Aualia <juniorsalimoaualia@gmail.com>
2025-11-17 12:17:00 +0200

    feat: adicionar a entidade Horario

---
+++ src/main/java/com/uem/sigapi/model/domain/Horario.java
@@ -0,0 +1,20 @@
+package com.uem.sigapi.model.domain;
+import lombok.*;
+import jakarta.persistence.Entity;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+@Entity(name="horarios")
+public class Horario {
+    private Curso curso;
+    private Turma turma;
+    private Disciplina disciplina;
+    private Docente docente;
+    private Sala sala;
+    private String diaSemana;
+    private String horaInicio;
+    private String horaFim;
+
+}
