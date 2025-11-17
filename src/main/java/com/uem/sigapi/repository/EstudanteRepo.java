3d2695883749c3487054c7d214425cb635a484ce
Salimo Aualia <juniorsalimoaualia@gmail.com>
2025-11-17 13:56:37 +0200

    feat: adicionar EstudanteRepo

---
+++ src/main/java/com/uem/sigapi/repository/EstudanteRepo.java
@@ -0,0 +1,37 @@
+package com.uem.sigapi.repository;
+
+import com.uem.sigapi.model.domain.Estudante;
+import com.uem.sigapi.model.domain.Curso;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstudanteRepo {
+
+    private static List<Estudante> estudantes = new ArrayList<>();
+    private CursoRepo cursoRepo = new CursoRepo();
+
+    public EstudanteRepo() {
+        estudantes.add(new Estudante("Carlos Nhaca", "Carlos", "carlos@uem.mz", "842300001", "Masculino", "2001-04-12", cursoRepo.findByNome("Engenharia Civil")));
+        estudantes.add(new Estudante("Fatima Nhate", "Fatima", "fatima@uem.mz", "842300002", "Feminino", "2002-07-23", cursoRepo.findByNome("Engenharia Informática")));
+        estudantes.add(new Estudante("João Chissano", "João", "joao@uem.mz", "842300003", "Masculino", "2000-11-01", cursoRepo.findByNome("Engenharia Mecânica")));
+        estudantes.add(new Estudante("Aminata Muchanga", "Aminata", "aminata@uem.mz", "842300004", "Feminino", "2001-02-17", cursoRepo.findByNome("Engenharia Ambiental")));
+        estudantes.add(new Estudante("Pedro Mbanze", "Pedro", "pedro@uem.mz", "842300005", "Masculino", "2003-05-29", cursoRepo.findByNome("Engenharia Química")));
+        estudantes.add(new Estudante("Ana Nqueto", "Ana", "ana@uem.mz", "842300006", "Feminino", "2002-09-14", cursoRepo.findByNome("Engenharia Eléctrica")));
+    }
+
+    public void save(Estudante estudante) {
+        estudantes.add(estudante);
+    }
+
+    public List<Estudante> findAll() {
+        return estudantes;
+    }
+
+    public Estudante findByNome(String nome) {
+        return estudantes.stream()
+                .filter(e -> e.getNome().equals(nome))
+                .findFirst()
+                .orElse(null);
+    }
+}
+
