75e627a858b9aaee2dfbdd9c0115cd180f51caba
Salimo Aualia <juniorsalimoaualia@gmail.com>
2025-11-17 12:19:58 +0200

    feat: adicionar a entidade Subscricao

---
+++ src/main/java/com/uem/sigapi/model/domain/Subscricao.java
@@ -0,0 +1,16 @@
+package com.uem.sigapi.model.domain;
+import lombok.*;
+import jakarta.persistence.Entity;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
+@Entity(name="subscricoes")
+public class Subscricao {
+    private Estudante estudante;
+    private String situacao;
+
+  
+}
+
