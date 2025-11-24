ada10917583083a3ec70180aa416a1ac334938a3
Salimo Aualia <juniorsalimoaualia@gmail.com>
2025-11-24 18:00:06 +0200

    Alterando a classe EstudanteRepo

--- src/main/java/com/uem/sigapi/repository/EstudanteRepo.java
+++ src/main/java/com/uem/sigapi/repository/EstudanteRepo.java
@@ -10,12 +10,12 @@ public class EstudanteRepo {
     private CursoRepo cursoRepo = new CursoRepo();
 
     public EstudanteRepo() {
-        estudantes.add(new Estudante("Carlos Nhaca", "Carlos", "carlos@uem.mz", "842300001", "Masculino", "2001-04-12", cursoRepo.findByNome("Engenharia Civil")));
-        estudantes.add(new Estudante("Fatima Nhate", "Fatima", "fatima@uem.mz", "842300002", "Feminino", "2002-07-23", cursoRepo.findByNome("Engenharia Informática")));
-        estudantes.add(new Estudante("João Chissano", "João", "joao@uem.mz", "842300003", "Masculino", "2000-11-01", cursoRepo.findByNome("Engenharia Mecânica")));
-        estudantes.add(new Estudante("Aminata Muchanga", "Aminata", "aminata@uem.mz", "842300004", "Feminino", "2001-02-17", cursoRepo.findByNome("Engenharia Ambiental")));
-        estudantes.add(new Estudante("Pedro Mbanze", "Pedro", "pedro@uem.mz", "842300005", "Masculino", "2003-05-29", cursoRepo.findByNome("Engenharia Química")));
-        estudantes.add(new Estudante("Ana Nqueto", "Ana", "ana@uem.mz", "842300006", "Feminino", "2002-09-14", cursoRepo.findByNome("Engenharia Eléctrica")));
+        estudantes.add(new Estudante("Carlos Nhaca", "Nhaca", "carlos@uem.mz", "842300001", "Masculino", "2001-04-12", cursoRepo.findByNome("Engenharia Civil")));
+        estudantes.add(new Estudante("Fatima Nhate", "Nhate", "fatima@uem.mz", "842300002", "Feminino", "2002-07-23", cursoRepo.findByNome("Engenharia Informática")));
+        estudantes.add(new Estudante("Joao Chissano", "Chissano", "joao@uem.mz", "842300003", "Masculino", "2000-11-01", cursoRepo.findByNome("Engenharia Mecânica")));
+        estudantes.add(new Estudante("Aminata Muchanga", "Muchanga", "aminata@uem.mz", "842300004", "Feminino", "2001-02-17", cursoRepo.findByNome("Engenharia Ambiental")));
+        estudantes.add(new Estudante("Pedro Mbanze", "Mbanze", "pedro@uem.mz", "842300005", "Masculino", "2003-05-29", cursoRepo.findByNome("Engenharia Química")));
+        estudantes.add(new Estudante("Ana Nqueto", "Nqueto", "ana@uem.mz", "842300006", "Feminino", "2002-09-14", cursoRepo.findByNome("Engenharia Eléctrica")));
     }
 
     public void save(Estudante estudante) {
